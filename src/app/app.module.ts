7921268c9ae98fe9fad4873ed2fe7783863c6c4f
sebastiansz1992 <sagudelo@kioniksolutions.com>
2019-06-20 22:48:29 -0500

    Generación de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SobremiComponent } from './components/sobremi/sobremi.component';
+import { AboutComponent } from './components/about/about.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { CreateComponent } from './components/create/create.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SobremiComponent,
+    AboutComponent,
+    ProjectsComponent,
+    CreateComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
